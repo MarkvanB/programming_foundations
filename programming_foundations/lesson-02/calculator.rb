3b94b52322760131a814123b5f62154a1b5e2602
Mark van Brecht <m.van.brecht@xlsglobal.com>
2016-04-04 11:01:02 +0200

    after exercise 2

--- programming_foundations/lesson-02/calculator.rb
+++ programming_foundations/lesson-02/calculator.rb
@@ -1,31 +1,112 @@
-# ask the user for two numbers
-# ask the user for an operation to perform
-# perform the operation on the two numbers
-# output the result
+LANGUAGE = 'en'
 
-# answer  = Kernel.gets()
-# Kernel.puts (answer)
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
 
-Kernel.puts("Welcome to Calculator!")
-
-Kernel.puts("What's the first number?")
-number1 = Kernel.gets().chomp()
-
-Kernel.puts("What's the second number?")
-number2 = Kernel.gets().chomp()
-
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-operator=Kernel.gets().chomp()
-
-if operator == '1'
-  result = number1.to_i() + number2.to_i()
-elsif operator == '2'
-  result = number1.to_i() - number2.to_i()
-elsif operator == '3'
-  result = number1.to_i() * number2.to_i()
-elsif operator == '4'
-  result = number1.to_f() / number2.to_f()  
+def messages(message, lang='en')
+  MESSAGES[lang][message]
 end
 
-Kernel.puts("The result is #{result}")
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
 
+def valid_number?(num)
+      num.to_i().to_s() == num
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+prompt(messages('welcome', LANGUAGE))
+
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt(messages('valid_name', LANGUAGE))
+  else
+    break
+  end
+end
+
+prompt(messages('greeting', LANGUAGE) + "#{name}!")
+
+loop do # main loop
+  number1 = ''
+  loop do
+    prompt(messages('first_number', LANGUAGE))
+    number1 = Kernel.gets().chomp()
+
+    if valid_number?(number1)
+      break
+    else
+      prompt(messages('number_error', LANGUAGE))
+    end
+  end
+
+  number2 = ''
+  loop do
+    prompt(messages('second_number', LANGUAGE))
+    number2 = Kernel.gets().chomp()
+
+    if valid_number?(number2)
+      break
+    else
+      prompt(messages('number_error', LANGUAGE))
+    end
+  end
+
+  operator_prompt = <<-MSG
+
+    1) +
+    2) -
+    3) *
+    4) /
+  MSG
+  prompt(messages('operator_question', LANGUAGE))
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3 or 4")
+    end
+  end
+
+  prompt("#{operation_to_message(operator)}" + messages('calculating_msg', LANGUAGE))
+
+  result =  case operator
+            when '1'
+              number1.to_i() + number2.to_i()
+            when '2'
+              number1.to_i() - number2.to_i()
+            when '3'
+              number1.to_i() * number2.to_i()
+            when '4'
+              number1.to_f() / number2.to_f()
+            end
+
+  prompt(messages('result', LANGUAGE) + "#{result}")
+
+  prompt(messages('again_msg', LANGUAGE))
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt(messages('goodbye', LANGUAGE))
---
+++ programming_foundations/lesson-02/mortage_calculator.rb
@@ -0,0 +1,74 @@
+# mortage_calculator.rb
+loop do
+system("clear")
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+puts "*************************************"
+puts "*                                   *"
+puts "* Welcome to the Mortage Calculator *"
+puts "*                                   *"
+puts "*************************************"
+puts
+
+# ask for loan amount
+
+prompt "What is the amount you want to loan?"
+
+amount = ''
+loop do
+amount = gets.chomp
+ if amount.to_s != amount.to_i.to_s
+  prompt "Please enter a valid number"
+ else
+  break      
+ end
+end    
+
+# bereken maandelijkse rente
+
+prompt "What is the Anuual interest rate in %?"
+prompt "Do not enter the %-sign (e.g. 4.5)"
+
+annual_interest = ''
+loop do
+annual_interest = gets.chomp
+  if /^[\d]+(\.[\d]+){0,1}$/ === annual_interest
+    break
+  else
+    prompt "pleae enter a valid interest rate (e.g. 4.5)"
+  end
+end      
+
+monthly_interest = (((1 + (annual_interest.to_f / 100)) ** (1.to_f / 12)) - 1).round(6)
+
+
+# bereken looptijd in maanden
+
+prompt "What is the loan duration (in years)?"
+
+duration_years = ''
+loop do
+duration_years = gets.chomp
+  if duration_years.to_s != duration_years.to_i.to_s
+    prompt "please enter a valid number"
+  else
+    break
+  end
+end
+
+duration_months = duration_years.to_i * 12
+
+
+# bereken maandelijkse aflossing
+
+monthly_payment = (amount.to_i * monthly_interest.to_f) / (1 - (1.to_f + monthly_interest.to_f)**(-duration_months.to_f)) 
+
+
+prompt "Your monthly payment is: $ " + monthly_payment.round(2).to_s
+prompt "would you like to make another calcultion? (Type Y for yes)"
+answer = gets.chomp
+break unless answer.downcase().start_with?('y')
+end
